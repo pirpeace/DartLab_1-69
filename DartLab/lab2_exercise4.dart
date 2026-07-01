a6a1771d930291e8e6ae9052228a535ce31f8388
pirpeace <104049642+pirpeace@users.noreply.github.com>
2026-07-01 10:50:21 +0700

    new file

---
+++ DartLab/lab2_exercise4.dart
@@ -0,0 +1,14 @@
+void main() {
+  String? middleName;
+  String city = 'Nakhon Sawan';
+
+  int middleNameLength = middleName?.length ?? 0;
+  print('Middle name length: $middleNameLength');
+
+  print('City length: ${city.length}');
+
+  print('Middle name before assignment: $middleName');
+
+  middleName ??= 'N/A';
+  print('Middle name after assignment: $middleName');
+}
